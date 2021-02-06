8ac43a1d51afde69a19eae74613bf03602b71263
Alexander Zhukov <36170455+adzhukov@users.noreply.github.com>
2021-02-07 00:39:08 +0300

    Fix multiline event types parsing

--- parser/normal.go
+++ parser/normal.go
@@ -48,37 +48,40 @@ func (p *Parser) normal() {
 			current.WeekType = calendar.Even
 		}
 
-		subjectValue := row.GetCell(p.Column + offsetSubject).Value
+		subjects := strings.Split(row.GetCell(p.Column+offsetSubject).Value, "\n")
 		classType := strings.Split(row.GetCell(p.Column+offsetType).Value, "\n")
 		lecturer := strings.Split(row.GetCell(p.Column+offsetLecturer).Value, "\n")
 		classroom := strings.Split(row.GetCell(p.Column+offsetClassroom).Value, "\n")
 
-		for i, subject := range strings.Split(subjectValue, "\n") {
+		for i, subject := range subjects {
 			if subject == "" {
 				continue
 			}
 
-			current.Subject = subject
 			current.ClassType = classType[0]
-			if i < len(classType) {
+			if len(subjects) == 1 {
+				current.ClassType = strings.Join(classType, "")
+			} else if i < len(classType) {
 				current.ClassType = classType[i]
 			}
 
 			current.Lecturer = lecturer[0]
-			if i < len(lecturer) {
+			if len(subjects) == 1 {
+				current.Lecturer = strings.Join(lecturer, ", ")
+			} else if i < len(lecturer) {
 				current.Lecturer = lecturer[i]
 			}
 
 			current.Classroom = classroom[0]
-			if i < len(classroom) {
+			if len(subjects) == 1 {
+				current.Classroom = strings.Join(classroom, " ")
+			} else if i < len(classroom) {
 				current.Classroom = classroom[i]
 			}
 
-			parsed := repeat.Parse(current.Subject)
+			parsed := repeat.Parse(subject)
 			current.Repeat = parsed.Rule
-			if parsed.Subject != "" {
-				current.Subject = parsed.Subject
-			}
+			current.Subject = parsed.Subject
 
 			setEventTime(&current, timeCell, parsed.StartWeek)
 			p.Calendar.Classes = append(p.Calendar.Classes, current)
